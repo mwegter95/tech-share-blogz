c9d7cdaa608f2855d2d6d8a2e330cde3ea7039c9
hitmaker <mwegter95@gmail.com>
2022-01-19 18:34:47 -0600

    added controllers/index.js and server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'munch munch yummy ginger ale',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+
+
+});
