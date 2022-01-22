6e4af89fe33b5322fe080a939921146526d7efca
zweets <mwegter95@gmail.com>
2022-01-22 15:45:37 -0700

    working homepage view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,6 @@ router.get('/', (req, res) => {
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -52,8 +51,7 @@ router.get('/post/:id', (req, res) => {
       'id',
       'post_url',
       'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'created_at'
     ],
     include: [
       {
