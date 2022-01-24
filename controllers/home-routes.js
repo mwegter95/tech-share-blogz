c6ce1d19c2755826f1d7c81f3e7674da7fba8193
zweets <mwegter95@gmail.com>
2022-01-23 20:14:40 -0700

    homepage added dashboard nav link, homepage good and in earliest on top sort

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+      const posts = dbPostData.reverse().map(post => post.get({ plain: true }));
 
       res.render('homepage', {
         posts,
